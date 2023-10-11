72056b5015ad01cb7cb1deef62a2fa0efd2cdf5d
Serge Junior Szynkarski <141464452+szynner@users.noreply.github.com>
2023-10-11 14:40:29 +0200

    Add files via upload

---
+++ Codecademy/Java/Learn-java/Conditionals-and-control-flow/Continents.java
@@ -0,0 +1,39 @@
+/**
+ * This class demonstrates the use of a switch statement to display the capital city
+ * of a continent based on a numeric code representing the continent.
+ */
+public class Continents {
+  public static void main(String[] args) {
+    int continent = 4;
+
+    // Use a switch statement to determine the continent and display the corresponding capital city.
+    switch (continent) {
+      case 1:
+        System.out.println("North America: Mexico City, Mexico");
+        break;
+      case 2:
+        System.out.println("South America: Sao Paulo, Brazil");
+        break;
+      case 3:
+        System.out.println("Europe: Moscow, Russia");
+        break;
+      case 4:
+        System.out.println("Africa: Lagos, Nigeria");
+        break;
+      case 5:
+        System.out.println("Asia: Shanghai, China");
+        break;
+      case 6:
+        System.out.println("Australia: Sydney, Australia");
+        break;
+      case 7:
+        System.out.println("Antarctica: McMurdo Station, US");
+        break;
+      default:
+        System.out.println("Undefined continent!");
+        break;
+    }
+  }
+}
+
+// Output: Africa: Lagos, Nigeria
